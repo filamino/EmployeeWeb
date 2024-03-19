7b4d6e643aa3af66d97ff695fdfa06f5b889addc
Filmon Kidane <fkidane@kabbee.org>
2024-03-18 20:06:13 -0500

    Admin Added

---
+++ EmployeeWeb/Models/Admin.cs
@@ -0,0 +1,17 @@
+﻿namespace EmployeeWeb.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+
+        public Admin(int id, string name, string role)
+        {
+            Id = id;
+            Name = name;
+            Role = role;
+        }
+
+    }
+}
